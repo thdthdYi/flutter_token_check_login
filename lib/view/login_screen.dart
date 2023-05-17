b483b6ef8b7a1ebbab95e1c2a80b197e7427bb49
hee <songyn111@gmail.com>
2023-05-17 17:20:54 +0900

    Storage이용하여 Token 저장(자동로그인)

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import 'nextpage.dart';
 
@@ -19,9 +20,12 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
+    //storage open > 프로그램 빌드할 때마다 로그인이 풀리는 것을 방지
+    final storage = FlutterSecureStorage(); //storage open
+
     final dio = Dio();
 
-    //localhost
+    //localhost ipㄴ
     final emulatorIp = '10.0.2.2:3000';
     final simulatorIp = '127.0.0.1:3000';
 
@@ -48,6 +52,7 @@ class _LoginScreenState extends State<LoginScreen> {
                     hintText: '아이디',
                   ),
                   onChanged: (String value) {
+                    //입력값 저장
                     username = value;
                   }),
               const SizedBox(
@@ -55,11 +60,12 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
               TextFormField(
                 decoration: InputDecoration(
-                  hintText: '아이디',
+                  hintText: '비밀번호',
                 ),
                 onChanged: (String value) {
                   password = value;
                 },
+                //hidden
                 obscureText: true,
               ),
               const SizedBox(
@@ -76,10 +82,20 @@ class _LoginScreenState extends State<LoginScreen> {
                   //rawString encode
                   String token = stringToBase64.encode(rawString);
 
-                  //로그인버튼을 누르면 해당 아이피로 요청
+                  //로그인버튼을 누르면 해당 아이피, path로 요청
                   final resp = await dio.post('http://$ip/$path',
                       options:
                           Options(headers: {'authorization': 'Basic $token'}));
+
+                  //resp에서 token 가져오기
+                  final refreshToken = resp.data['refreshToken'];
+                  final accessToken = resp.data['accessToken'];
+
+                  //저장소에 값 넣어주기
+                  await storage.write(
+                      key: 'REFRESH_TOKEN_KEY', value: refreshToken);
+                  await storage.write(
+                      key: 'ACCESS_TOKEN_KEY', value: accessToken);
 
                   //토큰 인증이 되면 다음 화면으로 넘어감
                   Navigator.of(context).push(
